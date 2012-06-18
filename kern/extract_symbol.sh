1dbb42bbb7841821e3c95ea835285bf6d84057fe
Andrea Bittau <bittau@cs.stanford.edu>
2012-06-18 16:46:03 -0700

    extract symbol

---
+++ kern/extract_symbol.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+KVER=`uname -r`
+SYSTEM_MAP=/boot/System.map-$KVER
+SYM=$1
+
+extract()
+{
+	grep $SYM $SYSTEM_MAP | head -n 1 | cut -d" " -f1
+	exit 0
+}
+
+check_file()
+{
+ls $1 2> /dev/null > /dev/null
+if [ $? -eq 0 ] ; then
+	extract
+fi
+}
+
+check_file $SYSTEM_MAP
+
+SYSTEM_MAP=/lib/modules/$KVER/build/System.map
+check_file $SYSTEM_MAP
+
+VMLINUX=/lib/modules/$KVER/build/vmlinux
+ls $VMLINUX 2> /dev/null > /dev/null
+if [ $? -eq 0 ] ; then
+	nm $VMLINUX | grep $SYM | egrep " (R|T) " | cut -d" " -f1
+	exit 0
+fi
+
+echo "FAILED - Can't find symbol!"
+exit 1
