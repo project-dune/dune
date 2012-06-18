d87ea622cf67a3cd62a723926f72c1176a460514
Adam Belay <abelay@stanford.edu>
2012-06-18 16:13:31 -0700

    more doc updates

--- dune_req.sh
+++ dune_req.sh
@@ -9,14 +9,14 @@ check_good()
 	fi
 }
 
-printf "Checking for VT-x... "
+printf "Checking for VT-x (needs EPT and VPID)... "
 cat /proc/cpuinfo | grep flags | grep ept | grep vpid | grep vmx > /dev/null
 check_good
 
-printf "Checking kernel version... "
+printf "Checking kernel version (needs 3.0 or later)... "
 uname -r | awk -F . '{print $1}' | grep 3 > /dev/null
 check_good
 
-printf "Checking for kernel headers... "
+printf "Checking for kernel headers (needs to be installed)... "
 ls /lib/modules/`uname -r`/source/include > /dev/null 2> /dev/null
 check_good
