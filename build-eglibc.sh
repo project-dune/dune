ec6ad8c26cb94624832e70df6aab362e66005c2e
Adam Belay <abelay@stanford.edu>
2012-07-02 14:11:29 -0700

    fix build for new eglibc patch

--- build-eglibc.sh
+++ build-eglibc.sh
@@ -4,7 +4,7 @@
 
 svn co svn://svn.eglibc.org/branches/eglibc-2_14 eglibc-2.14
 cd eglibc-2.14/libc
-cat ../../eglibc-2.14.diff | patch -p0 -d ./
+cat ../../eglibc-2.14.diff | patch -p2 -d ./
 mkdir ../eglibc-build
 cd ../eglibc-build
 ../libc/configure --disable-sanity-checks
