119941d2bd87cdd8ed88c36f6e503e18a7d0baf0
Ali Mashtizadeh <mashti@cs.stanford.edu>
2012-11-11 14:01:45 -0800

    Add test for time().

---
+++ test/timetest.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <time.h>
+
+#include "libdune/dune.h"
+
+int main(int argc, char *argv[])
+{
+	time_t t = -1;
+
+	int ret = dune_init_and_enter();
+	if (ret) {
+		printf("Failed to enter dune mode\n");
+		return ret;
+	}
+
+	t = time(&t);
+	if (t == -1) {
+		printf("Error calling time(&t)!");
+		return 1;
+	}
+
+	t = -1;
+	t = time(NULL);
+	if (t == -1) {
+		printf("Error calling time(NULL)!");
+		return 1;
+	}
+
+	printf("Success!");
+	return 0;
+}
+
