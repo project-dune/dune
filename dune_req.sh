590fe03723d2b3a546149f58fd77aeda3e7ea27f
Kostis Kaffes <kkaffes@stanford.edu>
2017-03-10 15:20:31 -0800

    Update requirement file for kernels 4.*
    Signed-off-by: Kostis Kaffes <kkaffes@stanford.edu>

--- dune_req.sh
+++ dune_req.sh
@@ -20,7 +20,7 @@ cat /proc/cpuinfo | grep flags | grep ept | grep vpid | grep vmx > /dev/null
 check_good
 
 printf "Checking kernel version (3.0 or later) ... "
-uname -r | awk -F . '{print $1}' | grep 3 > /dev/null 2> /dev/null
+uname -r | awk -F . '{print $1}' | grep '3\|4' > /dev/null 2> /dev/null
 check_good
 
 printf "Checking for kernel headers... "
