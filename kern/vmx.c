cfaac2bd9ba1a16fd45a5a46a6e6b66ea3398b00
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2017-10-27 10:18:05 -0200

    Removed VMX_EPT_EXTENT_INDIVIDUAL_ADDR
    Linux Kernel removed VMX_EPT_EXTENT_INDIVIDUAL_ADDR field.
    For more information look at the commit
    63f3ac48133a19110c8a3666028dbd9b1bf3dcb3 on the Linux Kernel repository

--- kern/vmx.c
+++ kern/vmx.c
@@ -180,10 +180,7 @@ static inline void ept_sync_context(u64 eptp)
 
 static inline void ept_sync_individual_addr(u64 eptp, gpa_t gpa)
 {
-	if (cpu_has_vmx_invept_individual_addr())
-		__invept(VMX_EPT_EXTENT_INDIVIDUAL_ADDR,
-				eptp, gpa);
-	else
+	if (!cpu_has_vmx_invept_individual_addr())
 		ept_sync_context(eptp);
 }
 
