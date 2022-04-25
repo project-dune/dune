530e97a16a4a29f5ce162ba745760c4ec05d581a
Zachary Yedidia <zyedidia@gmail.com>
2022-04-25 14:15:31 -0700

    Update extract symbol script

--- kern/extract_symbol.sh
+++ kern/extract_symbol.sh
@@ -1,34 +1,3 @@
 #!/bin/sh
 
-KVER=`uname -r`
-SYSTEM_MAP=/boot/System.map-$KVER
-SYM=$1
-
-extract()
-{
-	cat $SYSTEM_MAP | egrep " $SYM\$" | cut -d" " -f1
-	exit 0
-}
-
-check_file()
-{
-ls $1 2> /dev/null > /dev/null
-if [ $? -eq 0 ] ; then
-	extract
-fi
-}
-
-check_file $SYSTEM_MAP
-
-SYSTEM_MAP=/lib/modules/$KVER/build/System.map
-check_file $SYSTEM_MAP
-
-VMLINUX=/lib/modules/$KVER/build/vmlinux
-ls $VMLINUX 2> /dev/null > /dev/null
-if [ $? -eq 0 ] ; then
-	nm $VMLINUX | egrep " $SYM\$" | egrep " (R|T) " | cut -d" " -f1
-	exit 0
-fi
-
-echo "FAILED - Can't find symbol!"
-exit 1
+grep "\\b$1\\b" /proc/kallsyms | awk '{ print $1 }'
